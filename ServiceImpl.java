e30d14e2562ef6a15f0f2d08870013c9c91a762b
lihhd <66353495+lihhd@users.noreply.github.com>
2022-04-08 07:22:31 +0800

    Update ServiceImpl.java

--- ServiceImpl.java
+++ ServiceImpl.java
@@ -1,11 +1,11 @@
 @Service
 public class ServiceImpl implements Service { 
   @Autowired
-  private HandlerFactory handlerFactory;
+  private StrategyFactory strategyFactory;
   public CommonPair<String, Serializable> test1(@NonNull Request request) {
     String type = request.getType();
     // 根据 type 找到对应的提交处理器
-    Handler<Serializable> handler = handlerFactory.getHandler(type);
+    Handler<Serializable> handler = strategyFactory.getHandler(type);
     // 判断 type 对应的 handler 是否存在
     if (handler == null) {
       return CommonPair.failure("非法的提交类型: " + type);
